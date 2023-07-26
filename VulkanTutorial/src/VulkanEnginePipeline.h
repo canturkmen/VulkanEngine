3842ff0921e9c2b69ef054c2aa3947737215273c
Mehmet Can Turkmen <cturkmen@sabanciuniv.edu>
2023-07-26 10:13:54 +0300

    Setting up vulkan pipeline stages.

--- VulkanTutorial/src/VulkanEnginePipeline.h
+++ VulkanTutorial/src/VulkanEnginePipeline.h
@@ -9,7 +9,20 @@ namespace VulkanEngine {
 
 	struct PipelineConfigInfo
 	{
-		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo; 
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		std::vector<VkDynamicState> dynamicStateEnables;
+		VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
 	};
 	
 	class VulkanEnginePipeline 
